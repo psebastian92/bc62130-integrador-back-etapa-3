9a9a533f554c842296daec1e27ae4aa3d517cf67
Sebastian Munoz <sebastian.munoz092@gmail.com>
2023-11-20 20:27:03 -0300

    Termino controlador y modelo de productos

--- controllers/productos.controller.js
+++ controllers/productos.controller.js
@@ -1,25 +1,57 @@
-import models from '../models/productos.model.js'
+import models from "../models/productos.model.js";
 
 const obtenerProductos = async (req, res) => {
-  let id = req.params.id
+  let id = req.params.id;
 
-  if (id) {
-
-  } else {
-    const productos = await models.leerProductos()
-    return res.status(200).json(productos)
+  try {
+    if (id) {
+      const producto = await models.leerProducto(id);
+      return res.status(200).json(producto);
+    } else {
+      const productos = await models.leerProductos();
+      return res.status(200).json(productos);
+    }
+  } catch (error) {
+    console.log("No se pudo acceder a los productos", error);
+    res
+      .status(500)
+      .send("[obtenerProductos]: No se pudo acceder a los productos");
   }
-}
-
-
-const guardarProducto = (req, res) => {
-  res.send("POST (CREATE)");
 };
-const actualizarProducto = (req, res) => {
-  res.send("PUT (UPDATE)");
+
+const guardarProducto = async (req, res) => {
+  try {
+    const producto = req.body;
+    const productoGuardado = await models.guardarProducto(producto);
+    res.status(201).json(productoGuardado);
+  } catch (error) {
+    console.log("Error al guardar un producto", error);
+    return res.status(500).send("Error al guardar");
+  }
 };
-const borrarProducto = (req, res) => {
-  res.send("DELETE (DELETE)");
+
+const actualizarProducto = async (req, res) => {
+  const { id } = req.params;
+  const producto = req.body;
+
+  try {
+    const productoActualizado = await models.modificarProducto(id, producto);
+    res.status(200).json(productoActualizado);
+  } catch (error) {
+    console.log("No se pudo actualizar el producto", error);
+    res.status(500).send("No se pudo actualizar el producto");
+  }
+};
+const borrarProducto = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const productoBorrado = await models.eliminarProducto(id);
+    res.status(200).json(productoBorrado);
+  } catch (error) {
+    console.log("No se pudo borrar", error);
+    res.status(500).send("No se pudo borrar el producto");
+  }
 };
 
 export default {
--- models/productos.model.js
+++ models/productos.model.js
@@ -20,22 +20,51 @@ const ProductosModel = mongoose.model("productos", productosSchema);
 /* Métodos que nos van a servir de interfaz para interactuar con la DB */
 /* ------------------------------------------------------------------- */
 
-const leerProducto = (id) => {};
-
-const leerProductos = async () => {
+const leerProducto = async (id) => {
     try {
-        const productos = await ProductosModel.find({})
-        return productos
+      const producto = await ProductosModel.findById(id)
+      return producto
     } catch (error) {
-        console.log('[leerProductos]: algo no salio bien...',error)
+      console.log('No se pudo leer el producto con el id',error)
     }
 };
 
-const guardarProducto = (productoNuevo) => {};
+const leerProductos = async () => {
+  try {
+    const productos = await ProductosModel.find({});
+    return productos;
+  } catch (error) {
+    console.log("[leerProductos]: algo no salio bien...", error);
+  }
+};
 
-const modificarProducto = (id, productoAEditar) => {};
+const guardarProducto = async (productoNuevo) => {
+  try {
+    const productoAlmacenado = new ProductosModel(productoNuevo);
+    await productoAlmacenado.save();
+    return productoAlmacenado;
+  } catch (error) {
+    console.log('ERROR (GUARDAR PRODUCTOS), no se pudo guardar en la DB', error)
+  }
+};
 
-const eliminarProducto = (id) => {};
+const modificarProducto = async (id, productoAEditar) => {
+   try {
+    const productoModificado = await ProductosModel.findByIdAndUpdate(id, productoAEditar)
+    return productoModificado
+   } catch (error) {
+    console.log('ERROR[modificarProducto]: No se pudo actualizar el producto',error)
+   }
+};
+
+const eliminarProducto = async (id) => {
+  try {
+    const productoBorrado = await ProductosModel.findByIdAndDelete(id)
+    return productoBorrado
+  } catch (error) {
+    console.log('ERROR (ELIMINAR PRODUCTO) en la db', error)
+  }
+};
 
 export default {
   leerProducto,
